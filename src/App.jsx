8770ba62288fe422296dca150b09ffb7efc91878
alejaromerom <alejaromerom>
2023-02-10 18:10:01 -0500

    se está estructrurando el componente de pago de servicios publicos

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,29 @@
-import React from 'react'
-import { Inicio } from './components/Inicio';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom"
-import Login from '../schemas/Login';
+import React from "react";
+import { Inicio } from "./components/Inicio";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "../schemas/Login";
 
-import './App.css'
-import Retiro from './components/Retiro';
-
+import "./App.css";
+import Retiro from "./components/Retiro";
+import PagoServicios2 from "./components/PagoServicios2";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route exact path="/" element={<Login/>} />
-          <Route exact path="/login" element={<Login/>} />
-          <Route exact path="/inicio" element={<Inicio/>} />
-          <Route exact path="/retiro" element={<Retiro/>} />
+          <Route exact path="/" element={<Login />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/inicio" element={<Inicio />} />
+          <Route exact path="/retiro" element={<Retiro />} />
+          <Route
+            exact
+            path="/pagoserviciospublicos"
+            element={<PagoServicios2 />}
+          />
         </Routes>
       </Router>
     </div>
-  )
+  );
 }
-export default App
+export default App;
--- src/components/PagoServicios2.jsx
+++ src/components/PagoServicios2.jsx
@@ -1,7 +1,41 @@
+import { useState } from "react";
 import React from "react";
+import { Navbar } from "../layouts/Navbar";
+import "../styles/PagoServicios2.css";
 
 const PagoServicios2 = () => {
-  return <div>PagoServicios2</div>;
+  const [referenceNumber, setReferenceNumber] = useState("");
+  const [paymentAmount, setPaymentAmount] = useState(0);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Enviar los datos a un servidor, procesar el pago, etc.
+  };
+
+  return (
+    <div>
+      <Navbar />
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="reference">Número de Referencia:</label>
+        <input
+          type="text"
+          id="reference"
+          value={referenceNumber}
+          onChange={(event) => setReferenceNumber(event.target.value)}
+        />
+
+        <label htmlFor="amount">Monto a Pagar:</label>
+        <input
+          type="number"
+          id="amount"
+          value={paymentAmount}
+          onChange={(event) => setPaymentAmount(event.target.value)}
+        />
+
+        <button type="submit">Pagar Servicio</button>
+      </form>
+    </div>
+  );
 };
 
 export default PagoServicios2;
