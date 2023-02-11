59455497b7d432862dc904b87065b082b67e161c
alejaromerom <alejaromerom>
2023-02-10 20:29:27 -0500

    cambios de estructura pantalla de tarjetas de credito con estilos

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Login from "../schemas/Login";
 import "./App.css";
 import Retiro from "./components/Retiro";
 import PagoServicios2 from "./components/PagoServicios2";
+import TarjetasDeCredito from "./components/TarjetasDeCredito";
 
 function App() {
   return (
@@ -16,6 +17,12 @@ function App() {
           <Route exact path="/login" element={<Login />} />
           <Route exact path="/inicio" element={<Inicio />} />
           <Route exact path="/retiro" element={<Retiro />} />
+          <Route
+            exact
+            path="/tarjetasdecredito"
+            element={<TarjetasDeCredito />}
+          />
+
           <Route
             exact
             path="/pagoserviciospublicos"
--- src/components/TarjetasDeCredito.jsx
+++ src/components/TarjetasDeCredito.jsx
@@ -1,7 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
+import { Navbar } from "../layouts/Navbar";
+import "../styles/TarjetasDeCredito.css";
 
-const TarjetasDeCredito = () => {
-  return <div>TarjetasDeCredito</div>;
-};
+function TarjetasDeCredito() {
+  const [holderName, setHolderName] = useState("");
+  const [securityCode, setSecurityCode] = useState("");
+  const [cardNumber, setCardNumber] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Enviar los datos a un servidor, realizar la consulta, etc.
+  };
+
+  return (
+    <div className="contenedor-info">
+      <Navbar />
+      <div className="usuario-tarjeta-credito">
+        <div>
+          <form className="formulario-tarjeta-credito" onSubmit={handleSubmit}>
+            <div>
+              <label className="usuario-titular" htmlFor="holderName">
+                Nombre del Titular:
+              </label>
+              <input
+                type="text"
+                id="holderName"
+                value={holderName}
+                onChange={(event) => setHolderName(event.target.value)}
+              />
+            </div>
+            <div>
+              <label className="wrapper" htmlFor="securityCode">
+                Código de Seguridad:
+              </label>
+              <input
+                type="text"
+                id="securityCode"
+                value={securityCode}
+                onChange={(event) => setSecurityCode(event.target.value)}
+              />
+            </div>
+            <div>
+              <label htmlFor="cardNumber">Número de Tarjeta:</label>
+              <input
+                type="text"
+                id="cardNumber"
+                value={cardNumber}
+                onChange={(event) => setCardNumber(event.target.value)}
+              />
+            </div>
+            <div></div>
+            <img
+              className="img-cards"
+              src="/public/logos-avance-tarjeta-credito-min.png"
+              alt="logo-tarjeta-credito"
+            />
+
+            <button type="submit" className="consultar">
+              Consultar
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default TarjetasDeCredito;
