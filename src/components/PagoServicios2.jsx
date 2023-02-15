a5cc0a78923048204dfe54ddbebbb0decd6cf196
KevinG3511 <kevin.guevarevara@cumplimos.co>
2023-02-15 13:48:45 -0500

    pago de servicios funcional

--- src/components/PagoServicios2.jsx
+++ src/components/PagoServicios2.jsx
@@ -9,6 +9,7 @@ const PagoServicios2 = () => {
   const [referenceNumber, setReferenceNumber] = useState(0);
   const [monto, setMonto] = useState(0);
   const [descripcion, setDescripcion] = useState("");
+  const [verificacion, setVerificacion] = useState(false);
 
   const leerInputRefe = (e) => {
     setReferenceNumber(e.target.value);
@@ -21,8 +22,7 @@ const PagoServicios2 = () => {
   const leerInputDescrip = (e) => {
     setDescripcion(e.target.value);
   }
-
-  const fetchData = async () => {
+  const verificarData = async () => {
     try {
       if (referenceNumber <= 0) {
         toast.error('El Número Referencia no puede estar vacio', {
@@ -30,11 +30,59 @@ const PagoServicios2 = () => {
         });
         setMonto('')
       } else {
-        if ( monto <= 0) {
+
+        const datos = await fetch('http://127.0.0.1:8000/mi_api/recibo', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({
+            referencia: referenceNumber,
+            convenio: localStorage.getItem('tipoServicio')
+          }),
+        });
+        if (datos.ok) {
+          const data = await datos.json();
+          // console.log(typeof(data))
+          if (data == 'Referencia no existe' ) {
+            toast.error('No existe este servicio con esa referencia', {
+              position: toast.POSITION.TOP_RIGHT
+            });
+          }
+          
+          if (data.length > 0) {
+            toast.success('Verificacion exitosa', {
+              position: toast.POSITION.TOP_RIGHT
+            });
+            setVerificacion(true)
+            let datos1 = data[0][0].replace("$","")
+            let newMonto = datos1.replace(".","")
+            setMonto(parseInt(newMonto))
+          }
+        }
+
+      }
+    } catch (error) {
+      toast.error('Referencia no existe', {
+        position: toast.POSITION.TOP_CENTER
+      });
+      setMonto('')
+    }
+  }
+
+  const fetchData = async () => {
+    
+    try {
+      if (referenceNumber <= 0) {
+        toast.error('El Número Referencia no puede estar vacio', {
+          position: toast.POSITION.TOP_RIGHT
+        });
+        setMonto('')
+      } else {
+        if (monto <= 0) {
           toast.error('El monto debe ser mayor a 0', {
             position: toast.POSITION.TOP_RIGHT
           });
         } else {
+          
           const datos = await fetch('http://127.0.0.1:8000/mi_api/recaudos', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
@@ -82,21 +130,31 @@ const PagoServicios2 = () => {
             id="reference"
             onChange={leerInputRefe}
           />
-          <label htmlFor="descripcion">Descripcion:</label>
-          <input
-            type="text"
-            id="descripcion"
-            onChange={leerInputDescrip}
-          />
-          <label htmlFor="amount">Monto a Pagar:</label>
-          <input
-            type="number"
-            required
-            id="amount"
-            value={monto}
-            onChange={leerInputMonto}
-          />
-          <button onClick={fetchData}>Pagar Servicio</button>
+          {
+            verificacion == true ?
+              (<>
+                <label htmlFor="descripcion">Descripcion:</label>
+                <input
+                  type="text"
+                  id="descripcion"
+                  onChange={leerInputDescrip}
+                />
+                <label htmlFor="amount">Monto a Pagar:</label>
+                <input
+                  type="number"
+                  required
+                  id="amount"
+                  defaultValue={monto}
+                  placeholder={monto}
+                  readOnly
+                  // onChange={leerInputMonto}
+                />
+                <button onClick={fetchData}>Pagar Servicio</button>
+              </>) :
+              (<>
+                <button onClick={verificarData}>Verificar</button>
+              </>)
+          }
         </form>
       </div>
     </div >
